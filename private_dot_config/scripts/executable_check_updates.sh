c4fad392d311090635385cdc4f4f6fc8ef188c32
petrovicante6@gmail.com <petrovicante6@gmail.com>
2024-03-25 20:59:36 +0100

    - Include flatpak updates in check updates script

--- private_dot_config/scripts/executable_check_updates.sh
+++ private_dot_config/scripts/executable_check_updates.sh
@@ -3,10 +3,16 @@
 # Check for updates using zypper
 update_info=$(zypper lu)
 
-# Count the number of lines that represent updates, subtracting the header lines
-update_count=$(echo "$update_info" | grep '|' | wc -l)
-let update_count=update_count # Adjust this based on the actual output
+# Count the number of lines that represent updates, adjusting for accurate counting
+zypper_update_count=$(echo "$update_info" | grep -E 'v |v$' | wc -l)
+
+# Check for Flatpak updates
+flatpak_update_info=$(flatpak remote-ls --updates)
+flatpak_update_count=$(echo "$flatpak_update_info" | grep -v '^Ref' | wc -l)
+
+# Calculate total updates
+total_updates=$((zypper_update_count + flatpak_update_count -1))
 
 # Output for Waybar
-echo "$update_count " 
+echo "$total_updates " 
 
