eacf92e8af2cdc12dbcf8c3b3e9ad2009d6ab16b
petrovicante6@gmail.com <petrovicante6@gmail.com>
2024-03-31 10:31:40 +0200

    - changes

--- private_dot_config/scripts/executable_auto_change_wallpaper.sh
+++ private_dot_config/scripts/executable_auto_change_wallpaper.sh
@@ -6,7 +6,7 @@
 # NOTE: this script is in bash (not posix shell), because the RANDOM variable
 # we use is not defined in posix
 
-if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
+if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
 	echo "Usage:
 	$0 <dir containing images>"
 	exit 1
@@ -15,24 +15,24 @@ fi
 # Edit below to control the images transition
 export SWWW_TRANSITION_FPS=120
 export SWWW_TRANSITION_STEP=2
-export SWWW_TRANSITION="any" 
+export SWWW_TRANSITION="any"
 export SWWW_TRANSITION_DURATION=3
 
 # This controls (in seconds) when to switch to the next image
 INTERVAL=600
 
 while true; do
-	find "$1" -type f \
-		| while read -r img; do
+	find "$1" -type f |
+		while read -r img; do
 			echo "$((RANDOM % 1000)):$img"
-		done \
-		| sort -n | cut -d':' -f2- \
-		| while read -r img; do
-			swww img "$img"
+		done |
+		sort -n | cut -d':' -f2- |
+		while read -r img; do
 			sleep 3
+			swww img "$img"
+			sleep 2
 			wal -i "$img"
-			$HOME/.config/waybar/launch.sh &		
+			$HOME/.config/waybar/launch.sh &
 			sleep $INTERVAL
 		done
 done
-
